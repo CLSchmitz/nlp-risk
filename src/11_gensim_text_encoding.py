70379dd58abd483da01c444f76b78957c90fef07
Chris Schmitz <christopherl.schmitz@gmail.com>
2020-10-20 21:35:20 +0200

    final update for submission

--- src/11_gensim_text_encoding.py
+++
@@ -1,5 +0,0 @@
-import pandas as import pd
-import gensim
-
-df = pd.read_csv('../data/processed/data_clean.csv', sep = '|')
-model = gensim.models.KeyedVectors.load_word2vec_format('./model/GoogleNews-vectors-negative300.bin', binary=True)
--- src/20_spacy_evaluation.py
+++ src/20_spacy_evaluation.py
@@ -1,22 +1,40 @@
 import spacy
 import pandas as pd
 import glob 
+import string
 
 # Load trained model
 nlp = spacy.load('../models/spacy_text_classifier')
 
 # Load real supplier samples
-path = '../data/test'
-all_files = glob.glob(path + "/*.csv")
-li = []
-for filename in all_files:
-    df = pd.read_csv(filename, sep = '|', header = 0)
-    print(df)
-    li.append(df)
-df = pd.concat(li, axis=0, ignore_index=True)
+df = pd.read_excel('../data/test/domain_samples.xlsx')
 
-    # # test the saved model
-    # print("Loading from", output_dir)
-    # nlp2 = spacy.load(output_dir)
-    # doc2 = nlp2(test_text)
-    # print(test_text, doc2.cats)
+# Same cleaning process as training data
+def clean(t):
+
+    t = remove_between_brackets(t)
+    t = ''.join([c for c in t if c in string.ascii_letters + ' .'])
+    t = t.lower()
+
+    return t
+
+def remove_between_brackets(dirty):
+
+  clean = ""
+  bad = 0
+
+  for c in dirty:
+     if c == '{':
+        bad += 1
+     elif c == '}':
+        bad -= 1
+
+     if bad == 0:
+        clean += c
+
+  return clean
+
+df['article_text_clean'] = df.article_text.apply(lambda x: clean(x))
+df['predictions'] = df.article_text_clean.apply(lambda x: nlp(x).cats)
+
+df.to_excel('../results/test_evals.xlsx')
